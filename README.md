68ce2d7966e569c25f77c51ffe2f6ab8cb1f4815
Richard Brasil <82055788+richardbrasil1@users.noreply.github.com>
2022-09-09 18:32:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# desafio-saudacao
+ Projeto de Javascript básico para dar uma saudação ao usuario de acordo com o horário atual dele.
---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    if (hora >= 0 && hora < 12) {
+        //Bom dia
+        msg.innerHTML = `Bom dia! Agora são ${hora} horas.`
+        img.src = 'imagens/foto-manha.png'
+        document.body.style.background ='#ffb24a'
+        
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde        
+        msg.innerHTML = `Boa tarde! Agora são ${hora} horas.`
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.background = '#753859'
+    } else {
+        //Boa noite
+        msg.innerHTML = `Boa noite! Agora são ${horas} horas.`
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.background = '#13223F'
+    }
+}
